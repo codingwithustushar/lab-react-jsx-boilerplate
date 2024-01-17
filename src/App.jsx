5e6157cc5e75a36ec60e5748667fb90dbadb62a0
Tushar Shekhar <tusharshekhar@Tushars-MacBook-Air.local>
2024-01-17 23:06:16 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,46 @@ const imageData = ()=>{
   ]
   return data;
 }
+let data = imageData()
 
 function App() {
   // code here
+  let galleyStyle = {
+    textAlign: "center",
+    
+  }
+  let styleMydiv = {
+    display: "grid",
+    gridTemplateColumns:"repeat(2,1fr)",
+    margin:"20px",
+    gap:"100px",
+    placeItems: "center"
+
+  }
+  return(
+    <>
+    <h1 style ={galleyStyle}>Kalvium Gallery</h1>
+
+    <div style={styleMydiv}> 
+      <img src = {data[0].img} width={"400px"}/>
+      <img src = {data[1].img} width={"400px"}/>
+      <img src = {data[2].img} width={"400px"}/>
+      <img src = {data[3].img} width={"400px"}/>
+    </div>
+
+    {/* <div style = {styleMydiv}>
+      {data.map((el)=>(
+      <div>
+        <img src = {el.img} width={"300px"}/>
+      </div>
+      ))}
+    </div> */}
+
+
+
+    </>
+
+  )
 }
 
 export default App;
